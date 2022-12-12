0934a974051d605f6de07e7313e9fc4728213ad8
João Santos <118611278+vilelafps@users.noreply.github.com>
2022-12-12 19:43:11 -0300

    subindo o projeto

---
+++ omdb_api/lib/omdb_api.ex
@@ -0,0 +1,37 @@
+defmodule Omdb do
+  # Precisamos definir a URL da API OMDB e a sua chave de acesso,
+  # que você pode obter em https://www.omdbapi.com/
+
+  @api_key "SUA CHAVE API"
+  @omdb_api_url "http://www.omdbapi.com/"
+
+  # Agora, podemos definir uma função que faz a requisição à API
+  # e retorna o resultado em formato JSON
+  def fetch(title) do
+
+    # Criamos a URL da requisição adicionando o título do filme e a chave de acesso
+
+    # url = "#{@omdb_api_url}?apikey=#{@api_key}&s=#{URI.encode(title)}"
+    url = "#{@omdb_api_url}?apikey=#{@api_key}&t=#{title}"
+
+    response = HTTPoison.get!(url)
+    case response.status_code do
+      200 -> Poison.decode!(response.body)
+      _ -> %{}
+    end
+  end
+
+  # Casos abaixo de sucesso e erro
+  def search(title) do
+    cond do
+      is_nil(title) ->
+        {:error, "titulo nao pode ser nil"}
+      !is_bitstring(title) ->
+        {:error, "titulo deve ser String"}
+      String.trim(title) == "" ->
+        {:error, "titulo nao pode ser vazio"}
+      true ->
+        fetch(title)
+    end
+  end
+end
---
+++ omdb_api/test/omdb_api_test.exs
@@ -0,0 +1,31 @@
+defmodule OmdbApiTest do
+  use ExUnit.Case
+  doctest OmdbApi
+
+  defmodule MoviexTest do
+  use ExUnit.Case, async: true
+
+  test "deve retornar erro ao informar titulo nil" do
+    t = nil
+
+    resultado = OmdbApi.search(t)
+    assert {:error, "titulo não pode ser nil"} == resultado
+  end
+
+  test "deve retornar erro ao informar titulo que não seja string" do
+    t = 01010
+
+    resultado = OmdbApi.search(t)
+    assert {:error, "titulo deve ser String"} == resultado
+  end
+
+  test "deve retornar erro ao informar titulo vazio" do
+    t = ""
+
+    resultado = OmdbApiep.search(t)
+    assert {:error, "titulo nao pode ser vazio"} == resultado
+  end
+
+end
+
+end
